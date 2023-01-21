f97575a3aa45d4a27e9e7f67d393a20a6e4f05ec
khorakovaUnicorn <katerina.horakova@plus4u.net>
2023-01-21 21:12:31 +0100

    init message

---
+++ main.js
@@ -0,0 +1,13 @@
+// tady je místo pro náš program
+
+let nadpis = document.querySelector(".nadpis");
+nadpis.style.color = "red";
+
+let odstavec = document.querySelector(".odstavec");
+odstavec.style.fontSize = "20px";
+
+let obrazek = document.querySelector(".obrazek");
+obrazek.src = "pes.jpg";
+
+let ctverecek = document.querySelector(".ctverecek");
+ctverecek.style.marginLeft = "40px";
